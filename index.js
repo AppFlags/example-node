d129353c63c3316c7b86b9f5533fc62b1744e83a
Justin Capogna <jcapogna@gmail.com>
2021-06-22 17:09:34 -0500

    Upgrade to appflags 1.0.0 and update example for new SDK

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const AppFlags = require("appflags");
 
 console.info("AppFlags Node.js example project");
-AppFlags.initialize("YOUR_KEY_HERE");
+
+const user = {
+    key: Math.random().toString()  // key should be a non-changing string representing your user, like an ID or email
+}
+AppFlags.initialize("YOUR_KEY_HERE", user);
 
 console.info("Subscribing to feature flag. Will get current value and any changes.");
-AppFlags.onFlagChanged("test", enabled => {
-    console.log("Feature is " + (enabled ? "enabled" : "disabled"));
+
+AppFlags.onFlagChanged("test", flag => {
+    console.log("Feature is " + (flag.value ? "enabled" : "disabled"));
 });
